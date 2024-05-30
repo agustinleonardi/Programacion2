0d2b5ae1d2be41d5f8a1e48f4adfb68a3a924297
agustinleonardi <agustinleonardi5@gmail.com>
2024-05-30 14:35:56 -0300

    Implementacion medios de transporte

--- Unidad 4/Medios_De_Transporte/src/main/java/com/example/medios_de_transporte/service/MedioTransporteServiceImp.java
+++ Unidad 4/Medios_De_Transporte/src/main/java/com/example/medios_de_transporte/service/MedioTransporteServiceImp.java
@@ -2,17 +2,22 @@ package com.example.medios_de_transporte.service;
 
 import com.example.medios_de_transporte.clases.MedioTransporte;
 import com.example.medios_de_transporte.interfaces.MedioTransporteService;
+import com.example.medios_de_transporte.repository.MedioTransporteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
 public class MedioTransporteServiceImp implements MedioTransporteService {
+    @Autowired
+    private MedioTransporteRepository medioTransporteRepository;
+
     @Override
     public MedioTransporte saveMedioTransporte(MedioTransporte medioTransporte) {
-        return null;
+        return medioTransporteRepository.save(medioTransporte);
     }
 
     @Override
     public List<MedioTransporte> getAllMediosTransporte() {
-        return null;
+        return medioTransporteRepository.findAll();
     }
 }
