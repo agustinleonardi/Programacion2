abd7da5c83e419cca70e27a7b8a2bc2fff939e45
agustinleonardi <agustinleonardi5@gmail.com>
2024-05-30 14:50:32 -0300

    agregar @service

--- Unidad 4/Medios_De_Transporte/src/main/java/com/example/medios_de_transporte/service/MedioTransporteServiceImp.java
+++ Unidad 4/Medios_De_Transporte/src/main/java/com/example/medios_de_transporte/service/MedioTransporteServiceImp.java
@@ -4,9 +4,10 @@ import com.example.medios_de_transporte.clases.MedioTransporte;
 import com.example.medios_de_transporte.interfaces.MedioTransporteService;
 import com.example.medios_de_transporte.repository.MedioTransporteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Service
 public class MedioTransporteServiceImp implements MedioTransporteService {
     @Autowired
     private MedioTransporteRepository medioTransporteRepository;
