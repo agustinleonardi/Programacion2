e64bdbf1f6143f76d66cfe00a5a832f11ff32bdb
agustinleonardi <agustinleonardi5@gmail.com>
2024-05-30 14:53:50 -0300

    carga implementacion

--- Unidad 4/Medios_De_Transporte/src/main/java/com/example/medios_de_transporte/service/CargaServiceImp.java
+++ Unidad 4/Medios_De_Transporte/src/main/java/com/example/medios_de_transporte/service/CargaServiceImp.java
@@ -2,17 +2,23 @@ package com.example.medios_de_transporte.service;
 
 import com.example.medios_de_transporte.clases.Carga;
 import com.example.medios_de_transporte.interfaces.CargaService;
+import com.example.medios_de_transporte.repository.CargaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Service
 public class CargaServiceImp implements CargaService {
+    @Autowired
+    private CargaRepository cargaRepository;
+
     @Override
     public Carga saveCarga(Carga carga) {
-        return null;
+        return cargaRepository.save(carga);
     }
 
     @Override
     public List<Carga> getAllCargas() {
-        return null;
+        return cargaRepository.findAll();
     }
 }
